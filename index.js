dd973c34d2fab1944e96402641a012ee360ca8f2
Michael Leaney <mleaney@tokbox.com>
2016-10-04 22:32:03 +1100

    Add tests and fix a few bugs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = function parsePrice(price) {
-  return parseFloat(String(price).replace(/[^\d\.]/g, ''), 10);
+  return parseFloat(
+    String(price).match(/[\d\.]+/)[0], 10
+  );
 }
